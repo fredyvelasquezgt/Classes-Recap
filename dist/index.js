ad87999749c6497bf952ad668ad73dc93b91e7ff
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-08 16:56:26 -0600

    add loseLife method

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 class Player {
+score = 0;
+numLives = 10;
     constructor(first, last) {
         this.first = first;
         this.last = last;
@@ -6,13 +8,19 @@ class Player {
     taunt(){
         console.log('booyah!')
     }
+    loseLife() {
+        this.numLives -=1;
+    }
 }
 
 const player1 = new Player("blue", "steele");
-console.log(player1.first);
-console.log(player1.last);
+// console.log(player1.first);
+// console.log(player1.last);
+// console.log(player1)
+console.log(player1.numLives)
+player1.loseLife();
+console.log(player1.numLives)
 
 
-
-const player2 = new Player("charlie", "brown");
-console.log(player2);
+// const player2 = new Player("charlie", "brown");
+// console.log(player2);
