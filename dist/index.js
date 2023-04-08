1a793dc73957795ed7637894c699f932127042e8
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-08 16:38:44 -0600

    created Player class

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
-"use strict";
-console.log('its working!');
-console.log('wuuu');
+class Player {
+    taunt(){
+        console.log('booyah!')
+    }
+}
+
+const player1 = new Player;
+console.log(player1);
+
+
+const player2 = new Player;
+console.log(player2);
