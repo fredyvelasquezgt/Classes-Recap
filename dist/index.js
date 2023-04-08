c2c547db37f1b276d001f283553845168dfc765a
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-08 16:43:15 -0600

    add constructor and parameters

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
     taunt(){
         console.log('booyah!')
     }
 }
 
-const player1 = new Player;
-console.log(player1);
+const player1 = new Player("blue", "steele");
+console.log(player1.first);
+console.log(player1.last);
 
 
-const player2 = new Player;
+
+const player2 = new Player("charlie", "brown");
 console.log(player2);
