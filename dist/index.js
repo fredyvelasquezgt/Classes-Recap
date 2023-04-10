61ccf1a29602934443717e8a4d2c9e778c7a1879
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-10 11:42:34 -0600

    added parameter properties shorthand

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,25 @@
 "use strict";
+// class Player {
+//     public readonly first: string;
+//     public readonly last: string; //public: visibility readonly: cant change
+//     private score: number =0; //solo es accesible dentro de la clase y no desde afuera
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last;
+//     }
+//     private secretMethod():void {
+//         console.log('secret method')
+//     }
+// }
 class Player {
-    constructor(first, last) {
-        this.score = 0; //solo es accesible dentro de la clase y no desde afuera
+    //private score: number =0; //solo es accesible dentro de la clase y no desde afuera
+    constructor(first, last, score) {
         this.first = first;
         this.last = last;
+        this.score = score;
     }
     secretMethod() {
         console.log('secret method');
     }
 }
-const elton = new Player("elton", "steele");
+const elton = new Player("elton", "steele", 121);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 
-class Player {
-    public readonly first: string;
-    public readonly last: string; //public: visibility readonly: cant change
-    private score: number =0; //solo es accesible dentro de la clase y no desde afuera
+// class Player {
+//     public readonly first: string;
+//     public readonly last: string; //public: visibility readonly: cant change
+//     private score: number =0; //solo es accesible dentro de la clase y no desde afuera
 
-    constructor(first: string, last: string) {
-        this.first = first;
-        this.last = last;
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last;
+//     }
+
+//     private secretMethod():void {
+//         console.log('secret method')
+//     }
+// }
+
+class Player {
+    
+    //private score: number =0; //solo es accesible dentro de la clase y no desde afuera
+
+    constructor(public first:string, public last: string, private score: number) {
+
     }
 
     private secretMethod():void {
@@ -14,5 +27,7 @@ class Player {
     }
 }
 
-const elton = new Player("elton", "steele")
+
+const elton = new Player("elton", "steele", 121)
+
 
